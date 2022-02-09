21f4e2ea67869fd8d809ed5d53af48c736bbe2e5
vgj24 <vrushlijoshi24@gmail.com>
2022-02-09 15:16:28 +0530

    MoodAnalyser project

---
+++ MoodAnalyserAssgn/MoodAnalyser.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserAssgn
+{
+    public class MoodAnalyser
+    {
+        public string message;
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+        //default constructr
+        public MoodAnalyser()
+        {
+        }
+        public string AnalyseMood()
+        {
+            try
+            {
+                if (this.message.Equals(String.Empty))
+                {
+                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.EMPTY_MESSAGE, "Mood should not be empty");
+                }
+                if (this.message.Contains("SAD"))
+                    return "SAD";
+                else
+                    return "HAPPY";
+            }
+            catch (NullReferenceException obj)
+            {
+                //throw new Exception(obj.Message);
+
+                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NULL_MESSAGE, "Mood should not be null");
+            }
+            return null;
+        }
+    }
+}
+
+
