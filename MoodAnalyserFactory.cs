1dea777306c504cf397932cb993d3a01dbafa11a
vgj24 <vrushlijoshi24@gmail.com>
2022-02-09 15:00:24 +0530

    UC4_create factory class

---
+++ MoodAnalyserFactory.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace MoodAnalyserAssgn
+{
+    public class MoodAnalyserFactory
+    {
+        public string message;
+        public MoodAnalyserFactory(string message)
+        {
+            this.message = message;
+        }
+        public static object CreateMoodanalyser(string ClassName,string ConstructorName)
+        {
+            string pattern = @"." + ConstructorName + "$";
+            Match result=Regex.Match(ClassName, pattern);
+            if (result.Success)
+            {
+                try
+                {
+                    Assembly executing=Assembly.GetExecutingAssembly();
+                    Type moodAnalyseType = executing.GetType(ClassName);
+                    return Activator.CreateInstance(moodAnalyseType);
+                }
+                catch (ArgumentNullException)
+                {
+                    Console.WriteLine("your input is not valid");
+                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_CLASS, "class not found");
+                }
+                catch 
+                {
+                    Console.WriteLine("something wrong happened");
+                }
+            }
+            else
+            {
+                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_METHOD, "constructor not found");
+            }
+            return null;
+        }
+
+        public static object CreateMoodAnalyserUsingParameterizedConstructor(string className, string constructorName, string message)
+        {
+            Type type = typeof(MoodAnalyser);
+            if (type.Name.Equals(className) || type.FullName.Equals(className))
+            {
+                if (type.Name.Equals(constructorName))
+                {
+                    ConstructorInfo ctor = type.GetConstructor(new[] { typeof(string) });
+                    object instance = ctor.Invoke(new object[] { message });
+                    return instance;
+                }
+                else
+                {
+                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_METHOD, "Constructor is not found");
+                }
+            }
+            else
+            {
+                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_CLASS, "Class not found");
+
+            }
+        }
+    }
+}
